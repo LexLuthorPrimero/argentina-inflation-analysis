947cdbeb8f278775ab1eb0efaf92b013264ca1dc
Lucas <tu_email@gmail.com>
2026-04-20 21:12:23 -0300

    Agregar test simple de imports

---
+++ tests/test_simple.py
@@ -0,0 +1,6 @@
+def test_imports():
+    import pandas as pd
+    import requests
+    import matplotlib
+    import streamlit
+    assert True  # Si llegó hasta acá, todas las importaciones funcionaron
