9e15ebf9d98230e79a764d1161bfab2a1acf44d1
Lucas <tu_email@gmail.com>
2026-04-20 21:47:15 -0300

    Aplicar formato automático con ruff

--- src/utils/config.py
+++ src/utils/config.py
@@ -3,8 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 def get_bcra_token():
     return os.getenv("BCRA_API_TOKEN")
+
 
 DATA_RAW_PATH = "data/raw"
 DATA_PROCESSED_PATH = "data/processed"
