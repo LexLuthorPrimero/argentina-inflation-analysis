cb62aea03b692184f6949d7ce17126ccdbdbf75a
Lucas <tu_email@gmail.com>
2026-04-20 21:35:13 -0300

    Eliminar test_indicators.py definitivamente

--- tests/test_indicators.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import pandas as pd
-from src.transform.calculate import calcular_variacion_anual  # necesita ajustar import
-
-def test_calcular_variacion_anual():
-    datos = pd.DataFrame({'year': [2020,2021], 'inflacion_anual': [10,20]})
-    resultado = calcular_variacion_anual(datos)
-    assert resultado.loc[1, 'variacion'] == 100.0
